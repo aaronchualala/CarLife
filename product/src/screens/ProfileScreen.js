4176d13b94a090996d5a2431412f44a1c6b15691
Trigon25 <marccherndiyong2528@gmail.com>
2022-09-23 12:58:55 +0800

    Profile Page updates + Button component created

--- product/src/screens/ProfileScreen.js
+++ product/src/screens/ProfileScreen.js
@@ -1,15 +1,17 @@
-import {Text, View} from 'react-native';
+import {Text, View, ScrollView, Button, Alert} from 'react-native';
 import * as styles from '../css/ProfileScreen.module.css';
 import * as globalStyles from '../css/globals.css';
 import {MaterialIcon} from '../assets/MaterialIcons';
 import {Ionicon} from '../assets/Ionicons';
+import { OrangeButton } from '../components/Buttons';
 
 const ProfileScreen = () => {
   return (
-    <View style={{flex: 1, flexDirection:'column',justifyContent: 'flex-start', alignItems: 'center'}}>
-      <View style={globalStyles.banner}>
-        <Text style={globalStyles.bannerText}>Profile</Text>
-      </View>
+    <>
+    <View style={globalStyles.banner}>
+      <Text style={globalStyles.bannerText}>Profile</Text>
+    </View>
+    <ScrollView contentContainerStyle={styles.contentContainer}>
       <View id="accountContainer" style={styles.accountContainer}>
         <View id="profilePic" style={styles.profilePic}>
           <Ionicon
@@ -24,7 +26,15 @@ const ProfileScreen = () => {
           <Text style={styles.detailText}>Last Result: Gold</Text>
         </View>
       </View>
-    </View>
+      <View style={styles.nextIpptContainer}>
+        <View style={styles.ipptDateContainer}>
+          <Text style={styles.ipptDate}>Next IPPT: </Text>
+          <Text style={styles.ipptDateData}>01 Sept 2023</Text>
+        </View>          
+        <OrangeButton title="Change Date" onPress={() => Alert.alert('Simple Button pressed')} />
+      </View>
+    </ScrollView>
+    </>
   );
 };
 
