a8b2c82194d65782168628953b45562b6cbf4822
CodeLutetium <61930995+CodeLutetium@users.noreply.github.com>
2022-10-30 23:22:19 +0800

    css styling changes for cv

--- frontend/src/screens/PoseDetector.js
+++ frontend/src/screens/PoseDetector.js
@@ -7,6 +7,8 @@ import { Button, View, Text, StyleSheet, useWindowDimensions, ActivityIndicator,
 import { cameraWithTensors } from '@tensorflow/tfjs-react-native';
 import { Camera } from 'expo-camera';
 import { drawSkeletonPushUps, drawSkeletonSitUps, drawKeypoints } from "../utilities/draw";
+import * as styles from '../css/PoseDetector.module.css';
+import * as globalStyles from '../css/globals.css';
 
 export default function PoseDector(navigation) {
     // console.log(navigation);
@@ -71,8 +73,8 @@ function ModelView({type}){
 function LoadingView({ children }) {
     return (
       <View style={styles.loadingContainer}>
-        <View style={{ flexDirection: "row" }}>
-          <Text style={styles.loadingText}>{children}</Text>
+        <View style={{ flexDirection: "column" }}>
+          <Text style={styles.loadingText}>loading</Text>
           <ActivityIndicator />
         </View>
       </View>
@@ -315,109 +317,109 @@ function CustomTensorCamera({ style, width, ...props }) {
     );
 }
 
-  const styles = StyleSheet.create({
-    camera: {
-      zIndex: 0,
-    },
-    loadingContainer: { flex: 1, justifyContent: "center", alignItems: "center" },
-    loadingText: { fontSize: 18, textAlign: "center", marginRight: 8 },
-    predictionContainer: {
-        zIndex: 100,
-        position: "absolute",
-        bottom: 24,
-        left: 24,
-        right: 24,
-        backgroundColor: "rgba(255,255,255,0.8)",
-        padding: 8,
-        borderRadius: 20,
-        alignItems: "center",
-      },
-    predictionText: {
-        paddingVertical: 2,
-        fontSize: 20,
-    },
-    scoreContainer: {
-      zIndex: 100,
-      position: "absolute",
-      top: 24,
-      right: 24,
-      backgroundColor: "rgba(255,255,255,0.8)",
-      padding: 20,
-      borderRadius: 20,
-      alignItems: "center",
-    },
-    scoreText: {
-      paddingVertical: 2,
-      fontSize: 40,
-  },
-  feedbackContainer: {
-    zIndex: 100,
-    position: "absolute",
-    bottom: 24,
-    right: 10,
-    backgroundColor: "rgba(255,255,255,0.8)",
-    padding: 8,
-    borderRadius: 20,
-    alignItems: "center",
-  },
-  feedbackText: {
-    paddingVertical: 2,
-    fontSize: 20,
-  },
-  directionContainer: {
-    zIndex: 100,
-    position: "absolute",
-    top:24,
-    right:100,
-    backgroundColor: "rgba(255,255,255,0.8)",
-    padding: 8,
-    borderRadius: 20,
-    alignItems: "center",
-  },
-  directionText: {
-    paddingVertical: 2,
-    fontSize: 20,
-  },
-  timerContainer: {
-    zIndex: 100,
-    position: "absolute",
-    bottom:24,
-    left:10,
-    backgroundColor: "rgba(255,255,255,0.8)",
-    padding: 8,
-    borderRadius: 20,
-    alignItems: "center",
-  },
-  timerText: {
-    paddingVertical: 2,
-    fontSize: 30,
-  },
-  startContainer: {
-    zIndex: 1100,
-    position: "absolute",
-    justifyContent: "center",
-    left: 20,
-    backgroundColor: "rgba(255,255,255,0.8)",
-    padding: 8,
-    borderRadius: 20,
-    alignItems: "center",
-  },
-  startText: {
-    paddingVertical: 2,
-    fontSize: 30,
-  },
-  submitContainer: {
-    zIndex: 1100,
-    position: "absolute",
-    justifyContent: "center",
-    right: 20,
-    backgroundColor: "rgba(255,255,255,0.8)",
-    padding: 8,
-    borderRadius: 20,
-    alignItems: "center",
-  },
-  submitText: {
-    paddingVertical: 2,
-    fontSize: 30,
-  },
-  });
+  // const styles = StyleSheet.create({
+  //   camera: {
+  //     zIndex: 0,
+  //   },
+  //   loadingContainer: { flex: 1, justifyContent: "center", alignItems: "center" },
+  //   loadingText: { fontSize: 18, textAlign: "center", marginRight: 8 },
+  //   predictionContainer: {
+  //       zIndex: 100,
+  //       position: "absolute",
+  //       bottom: 24,
+  //       left: 24,
+  //       right: 24,
+  //       backgroundColor: "rgba(255,255,255,0.8)",
+  //       padding: 8,
+  //       borderRadius: 20,
+  //       alignItems: "center",
+  //     },
+  //   predictionText: {
+  //       paddingVertical: 2,
+  //       fontSize: 20,
+  //   },
+  //   scoreContainer: {
+  //     zIndex: 100,
+  //     position: "absolute",
+  //     top: 24,
+  //     right: 24,
+  //     backgroundColor: "rgba(255,255,255,0.8)",
+  //     padding: 20,
+  //     borderRadius: 20,
+  //     alignItems: "center",
+  //   },
+  //   scoreText: {
+  //     paddingVertical: 2,
+  //     fontSize: 40,
+  // },
+  // feedbackContainer: {
+  //   zIndex: 100,
+  //   position: "absolute",
+  //   bottom: 24,
+  //   right: 10,
+  //   backgroundColor: "rgba(255,255,255,0.8)",
+  //   padding: 8,
+  //   borderRadius: 20,
+  //   alignItems: "center",
+  // },
+  // feedbackText: {
+  //   paddingVertical: 2,
+  //   fontSize: 20,
+  // },
+  // directionContainer: {
+  //   zIndex: 100,
+  //   position: "absolute",
+  //   top:24,
+  //   right:100,
+  //   backgroundColor: "rgba(255,255,255,0.8)",
+  //   padding: 8,
+  //   borderRadius: 20,
+  //   alignItems: "center",
+  // },
+  // directionText: {
+  //   paddingVertical: 2,
+  //   fontSize: 20,
+  // },
+  // timerContainer: {
+  //   zIndex: 100,
+  //   position: "absolute",
+  //   bottom:24,
+  //   left:10,
+  //   backgroundColor: "rgba(255,255,255,0.8)",
+  //   padding: 8,
+  //   borderRadius: 20,
+  //   alignItems: "center",
+  // },
+  // timerText: {
+  //   paddingVertical: 2,
+  //   fontSize: 30,
+  // },
+  // startContainer: {
+  //   zIndex: 1100,
+  //   position: "absolute",
+  //   justifyContent: "center",
+  //   left: 20,
+  //   backgroundColor: "rgba(255,255,255,0.8)",
+  //   padding: 8,
+  //   borderRadius: 20,
+  //   alignItems: "center",
+  // },
+  // startText: {
+  //   paddingVertical: 2,
+  //   fontSize: 30,
+  // },
+  // submitContainer: {
+  //   zIndex: 1100,
+  //   position: "absolute",
+  //   justifyContent: "center",
+  //   right: 20,
+  //   backgroundColor: "rgba(255,255,255,0.8)",
+  //   padding: 8,
+  //   borderRadius: 20,
+  //   alignItems: "center",
+  // },
+  // submitText: {
+  //   paddingVertical: 2,
+  //   fontSize: 30,
+  // },
+  // });
