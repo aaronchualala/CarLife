cc016f66c74c8feb74c1733df98956792e450026
Aaron Chua <81175540+aaronchualala@users.noreply.github.com>
2022-11-04 01:06:56 +0800

    Remove firebase and product folders

--- product/src/components/CustomStatusBar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {getStatusBarHeight} from 'react-native-status-bar-height';
-import {View, Platform} from 'react-native';
-
-const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? getStatusBarHeight() : 0;
-
-export const CustomStatusBar = () => {
-  return <View style={{height: STATUSBAR_HEIGHT}} />;
-};
